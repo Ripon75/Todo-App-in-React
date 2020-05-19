d4a656342c97e8a283b4ad6181723929d8db5957
riponahmed <riponahmedjrtr@gmail.com>
2020-05-19 09:44:54 +0600

    Updates

---
+++ precache-manifest.db4011c2147686670036617a5956eef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3448fc0d04cb31fb15a9b6847d0733dd",
+    "url": "/Todo-App-in-React/index.html"
+  },
+  {
+    "revision": "633fd72012c2d11b52e5",
+    "url": "/Todo-App-in-React/static/css/main.09590e83.chunk.css"
+  },
+  {
+    "revision": "3fc45cb79dcc1c90fc40",
+    "url": "/Todo-App-in-React/static/js/2.7db60da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App-in-React/static/js/2.7db60da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633fd72012c2d11b52e5",
+    "url": "/Todo-App-in-React/static/js/main.63fd7060.chunk.js"
+  },
+  {
+    "revision": "f88ee1a265c05da0f2f7",
+    "url": "/Todo-App-in-React/static/js/runtime-main.2c820fae.js"
+  }
+]);
